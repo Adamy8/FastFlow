4b3327fede1372d5e9fc02f36efed8cdbcc4328b
Adamy <admye@ucdavis.edu>
2025-02-15 19:18:29 -0800

    authorization - login/register

--- fastapi/api/routers/auth.py
+++ fastapi/api/routers/auth.py
@@ -1,1 +1,54 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException, status
+from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordRequestForm  # OAuth2PasswordBearer
+from jose import jwt
+from dotenv import load_dotenv
+import os
+from api.models import User
+from api.deps import db_dependency, bcrypt_context
+
+load_dotenv()
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"],
+)
+
+SECRET_KEY = os.getenv("AUTH_SECRET_KEY")
+ALGORITHM = os.getenv("AUTH_ALGORITHM")
+
+class UserCreateRequest(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {'sub': username, 'id': user_id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({'exp': expires})
+    encoded_jwt = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], 
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,
+                            detail = "Incorrect username or password")
+    token = create_access_token(user.username, user.id, timedelta(minutes=20)) # 20 minutes for token expiration
+
+    return {"access_token": token, "token_type": "bearer"}
