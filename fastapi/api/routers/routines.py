5770b857c7ab5d0602ba7f785f76401477a756fc
Adamy <admye@ucdavis.edu>
2025-02-16 19:32:08 -0800

    routine router

---
+++ fastapi/api/routers/routines.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from fastapi import APIRouter
+from sqlalchemy.orm import joinedload
+from api.models import Workout, Routine
+from api.deps import db_dependency, user_dependency
+
+router = APIRouter(
+    prefix="/routines",
+    tags=["routines"],
+    # dependencies=[user_dependency]
+)
+
+class RoutineBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+class RoutineCreate(RoutineBase):
+    workouts: List[int] = []
+
+@router.get("/")
+def get_routines(db: db_dependency, user: user_dependency):
+    return db.query(Routine).options(joinedload(Routine.workouts)).filter(Routine.user_id == user.get('id')).all()
+
+@router.post("/")
+def create_routine(db: db_dependency, user: user_dependency, routine: RoutineCreate):
+    new_routine = Routine(name=routine.name, description=routine.description, user_id=user.get('id'))
+    for workout_id in routine.workouts:
+        workout = db.query(Workout).filter(Workout.id == workout_id).first()
+        if workout:
+            new_routine.workouts.append(workout)
+    db.add(new_routine)
+    db.commit()
+    db.refresh(new_routine)
+    new_routine = db.query(Routine).options(joinedload(Routine.workouts)).filter(Routine.id == new_routine.id).first()
+    return new_routine
+
+@router.delete("/")
+def delete_routine(db: db_dependency, user: user_dependency, routine_id: int):
+    routine = db.query(Routine).filter(Routine.id == routine_id).first()        # Routine.user_id == user.get('id')
+    if routine:
+        db.delete(routine)
+        db.commit()
+    return routine
